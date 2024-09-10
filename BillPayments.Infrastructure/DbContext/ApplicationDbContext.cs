dc12f4c536fcadc86545e5269b7876d90270098f
k-tohid <130159541+k-tohid@users.noreply.github.com>
2024-09-10 16:05:09 +0330

    create Infrastructure layer and ApplicationDbContext file

---
+++ BillPayments.Infrastructure/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using BillPayments.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillPayments.Infrastructure.DbContext
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, int>
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            :base(options)
+        {   
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Document> Documents { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
