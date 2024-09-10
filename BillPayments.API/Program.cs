6f08874728de66d2e500d8fd1c8530d403b40abd
k-tohid <130159541+k-tohid@users.noreply.github.com>
2024-09-10 16:28:15 +0330

    add startup services extention

--- BillPayments.API/Program.cs
+++ BillPayments.API/Program.cs
@@ -1,3 +1,5 @@
+using BillPayments.API.StartupExtentions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// startup services extention
+builder.Services.ConfigureServices(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ BillPayments.API/StartupExtentions/ConfigureServicesExtention.cs
@@ -0,0 +1,42 @@
+﻿using BillPayments.Core.Entities;
+using BillPayments.Infrastructure.DbContext;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BillPayments.API.StartupExtentions
+{
+    public static class ConfigureServicesExtention
+    {
+        public static IServiceCollection ConfigureServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+
+            // DbContext
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            // Identity
+            services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+            {
+                options.Password.RequiredLength = 5;
+                options.Password.RequiredUniqueChars = 1;
+
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+            })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders()
+                .AddUserStore<UserStore<ApplicationUser, ApplicationRole, ApplicationDbContext, int>>()
+                .AddRoleStore<RoleStore<ApplicationRole, ApplicationDbContext, int>>();
+
+
+            return services;
+        }
+    }
+}
