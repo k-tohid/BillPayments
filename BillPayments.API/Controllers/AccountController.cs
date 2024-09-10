895cc3ff95fa943c7f1a4b6cfb234c67ff3e69a5
k-tohid <130159541+k-tohid@users.noreply.github.com>
2024-09-10 16:33:41 +0330

    create account controller and delete default  weather template

---
+++ BillPayments.API/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BillPayments.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet(Name = "SayHello")]
+        public IActionResult GetHelloWorld()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
