2b9a1d71d09f508ad56d7e59d2438eba972c10f1
k-tohid <130159541+k-tohid@users.noreply.github.com>
2024-09-10 17:08:07 +0330

    fix document and category entities cascade problem in sql

--- BillPayments.Core/Entities/Category.cs
+++ BillPayments.Core/Entities/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,14 +17,19 @@ namespace BillPayments.Core.Entities
         public DateTime? ModifiedAt { get; set; }
 
         // Foreign keys to ApplicationUser for CreatedBy and ModifiedBy
-        public string CreatedById { get; set; }
-        public ApplicationUser CreatedBy { get; set; }
+        [ForeignKey(nameof(ApplicationUser))]
+        public int? CreatedById { get; set; }
+        public ApplicationUser? CreatedBy { get; set; }
 
-        public string? ModifiedById { get; set; }
+
+        [ForeignKey(nameof(ApplicationUser))]
+        public int? ModifiedById { get; set; }
         public ApplicationUser? ModifiedBy { get; set; }
 
+
+        [ForeignKey(nameof(Category))]
         public int? ParentId { get; set; }
-        public Category Parent { get; set; }
+        public Category? Parent { get; set; }
         public ICollection<Category> SubCategories { get; set; }
     }
 }
--- BillPayments.Core/Entities/Document.cs
+++ BillPayments.Core/Entities/Document.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,18 +10,21 @@ namespace BillPayments.Core.Entities
     public class Document
     {
         public int Id { get; set; }
-        public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public int? CategoryId { get; set; }
+        public Category? Category { get; set; }
         public string Title { get; set; }
         public string? Description { get; set; }
         public DateTime CreatedOn { get; set; }
         public DateTime? ModifiedAt { get; set; }
 
-        // Foreign keys to ApplicationUser for CreatedBy and ModifiedBy
-        public string CreatedById { get; set; }
-        public ApplicationUser CreatedBy { get; set; }
 
-        public string? ModifiedById { get; set; }
+        [ForeignKey(nameof(ApplicationUser))]
+        public int? CreatedById { get; set; }
+        public ApplicationUser? CreatedBy { get; set; }
+
+
+        [ForeignKey(nameof(ApplicationUser))]
+        public int? ModifiedById { get; set; }
         public ApplicationUser? ModifiedBy { get; set; }
 
         public ICollection<string> Attachments { get; set; }
