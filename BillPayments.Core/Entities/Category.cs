1bddd8c3f92d85596ca7abe528109cc00fcd1bb0
k-tohid <130159541+k-tohid@users.noreply.github.com>
2024-09-10 15:52:14 +0330

    create Category Entity

---
+++ BillPayments.Core/Entities/Category.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillPayments.Core.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsActive { get; set; }
+        public string? Thumbnail { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+
+        // Foreign keys to ApplicationUser for CreatedBy and ModifiedBy
+        public string CreatedById { get; set; }
+        public ApplicationUser CreatedBy { get; set; }
+
+        public string? ModifiedById { get; set; }
+        public ApplicationUser? ModifiedBy { get; set; }
+
+        public int? ParentId { get; set; }
+        public Category Parent { get; set; }
+        public ICollection<Category> SubCategories { get; set; }
+    }
+}
